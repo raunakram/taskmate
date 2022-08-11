2de07d14b21cb3c635cf6a4f64857c4df893bf05
raunak <raunakron4@gmail.com>
2022-08-11 18:39:22 +0530

    Uploading Taskmate Project

---
+++ todolist_app/views.py
@@ -0,0 +1,95 @@
+from email import message
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from todolist_app.models import TaskList
+from todolist_app.form import TaskForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def todolist(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.manage = request.user
+            instance.save()
+        messages.success(request,("New Task Added"))
+        return redirect('todolist')
+    else:
+        all_tasks = TaskList.objects.filter(manage=request.user)
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
+        return render(request, 'todolist.html', {'all_tasks': all_tasks })
+
+
+def about(request):
+
+    context= {
+
+        'welcome_text' :"Welcome to about..."
+    }
+    return render(request, 'about.html', context )
+
+
+def index(request):
+
+    context= {
+
+        'index_text' :"Welcome to index..."
+    }
+    return render(request, 'index.html', context )
+
+
+
+def contact(request):
+
+    context= {
+
+        'welcome_text' :"Welcome to contact page..."
+    }
+    return render(request, 'contact.html', context )
+
+@login_required
+def delete_task(request, task_id):
+    task= TaskList.objects.get(pk= task_id)
+    if task.manage == request.user:
+        task.delete()
+    else:
+        messages.error(request,('Access Denied!'))
+    
+    return redirect('todolist')
+
+
+@login_required
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task= TaskList.objects.get(pk= task_id)
+        form = TaskForm(request.POST or None, instance = task )
+        if form.is_valid():
+            form.save()
+        messages.success(request,("Task Edited"))
+        return redirect('todolist')
+    else:
+        task_obj = TaskList.objects.get(pk= task_id)
+        return render(request, 'edit.html', {'task_obj': task_obj })
+
+@login_required
+def complete_task(request, task_id):
+    task= TaskList.objects.get(pk= task_id)
+    if task.manage == request.user:
+        task.done = True
+        task.save()
+    else:
+        messages.error(request,('Access Denied!'))
+    return redirect('todolist')
+
+@login_required
+def pending_task(request, task_id):
+    task= TaskList.objects.get(pk= task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
---
+++ users_app/views.py
@@ -0,0 +1,17 @@
+from email import message
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .forms import CustomRegistrForm
+from django.contrib import messages
+# Create your views here.
+def register(request):
+    if request.method=="POST":
+        register_form = CustomRegistrForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New user created... Login to get started!"))
+            return redirect('register')
+    else:
+        register_form = CustomRegistrForm()
+    return render(request, 'register.html', {'register_form' : register_form})
+    
