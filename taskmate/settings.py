f436f1262d876963599f9b1b1ce34d8aa3e5d174
raunak <raunakron4@gmail.com>
2022-08-11 19:31:25 +0530

    Project Updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -14,6 +14,7 @@ from ensurepip import bootstrap
 import os
 from pathlib import Path
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "todolist"
 
 LOGIN_URL = "login"
+
+
+django_heroku.settings(locals())
